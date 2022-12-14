5e97af3a48df7e4b877b71f47dcaf33275ad75f8
val <you@ex.com>
2022-12-14 13:22:00 +0530

    added_command_values

--- app.py
+++ app.py
@@ -3,12 +3,22 @@ import telebot
 TOKEN = "5823312097:AAEk7BcZ2dP7zqghFaI5pbwik6eyvsO7BOY"
 
 bot = telebot.TeleBot(TOKEN)
-
+keys = {'биткоин': 'BTC',
+        'эфириум': 'ETH',
+        'доллар': 'USD',
+}
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
-    text = "Чтобы начать работу введите команду боту в следующем формате:\n <имя валюты> \
-    <в какую валюту перевести>\
-    <количество переводимой валюты>"
+    text = "Чтобы начать работу введите команду боту в следующем формате:\n\n <имя валюты> \
+<в какую валюту перевести> \
+<количество переводимой валюты> \n\n Увидеть список всех доступных валют команда - /values"
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key,))
     bot.reply_to(message, text)
 
 bot.polling()
