51b919b68930afdcfbbd711ef6ff9c563e59e01d
val <you@ex.com>
2022-12-14 16:14:27 +0530

    added_convertor

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+import requests
 import telebot
 
 TOKEN = "5823312097:AAEk7BcZ2dP7zqghFaI5pbwik6eyvsO7BOY"
@@ -6,6 +8,7 @@ bot = telebot.TeleBot(TOKEN)
 keys = {'биткоин': 'BTC',
         'эфириум': 'ETH',
         'доллар': 'USD',
+        'тон': 'TONCOIN'
 }
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
@@ -20,5 +23,13 @@ def values(message: telebot.types.Message):
     for key in keys.keys():
         text = '\n'.join((text, key,))
     bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text',])
+def convert(message: telebot.types.Message):
+    quote, base, amount = message.text.split(' ')
+    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}')
+    total_base = json.loads(r.content)[keys[base]]
+    text = f'Цена{amount} {quote} в {base} - {total_base}'
+    bot.send_message(message.chat.id, text)
 
 bot.polling()
