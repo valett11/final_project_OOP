ed524f52b59024fefcb235727a17d4e4abbda164
val <you@ex.com>
2022-12-20 23:58:08 +0530

    update_get_price

--- extensions.py
+++ extensions.py
@@ -7,7 +7,7 @@ class ConvertionException(Exception):
 
 class CryptoConverter:
     @staticmethod
-    def get_price(quote: str, base: str, amount: str):
+    def get_price(base: str, quote: str, amount: str):
         if quote == base:
             raise ConvertionException(f'Не удалось перевести одинаковые валюты {base}.')
 
@@ -26,7 +26,8 @@ class CryptoConverter:
         except KeyError:
             raise ConvertionException(f'Не удалось обработать количество {amount}.')
 
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        total_base = json.loads(r.content)[keys[base]] * amount
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+
+        total_base = json.loads(r.content)[keys[quote]] * amount
 
         return total_base
